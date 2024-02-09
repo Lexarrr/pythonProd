fd5fac81f832e274b65dce47c43e4114ee48519b
Lexa <etc.21@yandex.ru>
2024-02-09 21:23:13 +0300

    new file

--- obmennik2.py
+++ obmennik2.py
@@ -3,7 +3,7 @@ import requests
 INVALUTE = input("Введите валюту, которую вы хотитие поменять: ")
 OUTVALUTE = input("Введите валюту, на которую вы хотите поменять: ")
 INVALUTE_COUNT = float(input("Сколько Вы хотите поменять? : "))
-get_str = f'http://127.0.0.1:8000/convert?fv={INVALUTE}&sv={OUTVALUTE}&vcount={INVALUTE_COUNT}'
+get_str = f'http://172.20.10.13:8008/convert?fv={INVALUTE}&sv={OUTVALUTE}&vcount={INVALUTE_COUNT}'
 print(get_str)
 OUTVALUTE_COUNT = requests.get(get_str).json()
 print(f"Клиенту нужно выдать {OUTVALUTE_COUNT} {OUTVALUTE}")
